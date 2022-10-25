cd16acb13d0ac0d2b6af74e7e6ef1f2fb16d76f4
Marcelo Moll <mollmarcelomoll@gmail.com>
2022-10-25 20:18:04 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Coche coche = new Coche("Mercedes Benz",2,250,100);
+        System.out.println("Marca del auto : "+coche.marca);
+        System.out.println("Cantidad de puertas : "+coche.numeroDePuertas);
+        System.out.println("Velocidad maxima : "+coche.velocidadMaxima);
+        System.out.println("Velocidad actual : "+coche.velocidadActual);
+    }
+}
+class Coche {
+    String marca;
+    int numeroDePuertas;
+    int velocidadMaxima;
+    float velocidadActual;
+
+    public Coche(String marcaC, int puertas, int velocidad, int velocidadActua) {
+        marca = marcaC;
+        numeroDePuertas = puertas;
+        velocidadMaxima = velocidad;
+        velocidadActual = velocidadActua;
+        System.out.println("Estoy en el constructor");
+    }
+}
