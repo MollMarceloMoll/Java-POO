3faa2f16bcd8c4cdf7be372dd8c7ab5229d7766e
Marcelo Moll <mollmarcelomoll@gmail.com>
2022-10-26 10:27:04 -0300

    26-10-2022
    Metodos Set and Get

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,49 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Coche coche = new Coche("Mercedes Benz",2,250,100);
-        System.out.println("Marca del auto : "+coche.marca);
-        System.out.println("Cantidad de puertas : "+coche.numeroDePuertas);
-        System.out.println("Velocidad maxima : "+coche.velocidadMaxima);
-        System.out.println("Velocidad actual : "+coche.velocidadActual);
+        Vehiculo coche = new Vehiculo();
+        coche.setTipo("Coupe");
+        coche.setVelocidadMaxima(120);
+        coche.setRapido(true);
+
+        System.out.println(coche.getTipo());
+        System.out.println(coche.getVelocidadMaxima());
+        System.out.println(coche.getRapido());
+
+        Vehiculo moto = new Vehiculo();
+        moto.setTipo("Scotter");
+        moto.setVelocidadMaxima(50);
+        moto.setRapido(false);
+
+        System.out.println(moto.getTipo());
+        System.out.println(moto.getVelocidadMaxima());
+        System.out.println(moto.getRapido());
+
     }
 }
-class Coche {
-    String marca;
-    int numeroDePuertas;
-    int velocidadMaxima;
-    float velocidadActual;
+class Vehiculo {
+    private String tipo;
+    private int velocidadMaxima;
+    private boolean rapido;
 
-    public Coche(String marcaC, int puertas, int velocidad, int velocidadActua) {
-        marca = marcaC;
-        numeroDePuertas = puertas;
-        velocidadMaxima = velocidad;
-        velocidadActual = velocidadActua;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    public int getVelocidadMaxima() {
+        return this.velocidadMaxima;
+    }
+
+    public void setRapido(boolean rapido) {
+        this.rapido = rapido;
+    }
+    public boolean getRapido() {
+        return this.rapido;
     }
 }
